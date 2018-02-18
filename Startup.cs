57d6cdc71d4e1a3d39571f5245af2351ef067654
Grayson Gilmore <gilmoreg@live.com>
2018-02-18 13:35:52 -0800

    Add Auth0 services to startup

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -23,7 +24,18 @@ namespace referendus_netcore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
+			// 1. Add Authentication Services
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+			}).AddJwtBearer(options =>
+			{
+				options.Authority = "https://gilmoreg.auth0.com/";
+				options.Audience = "referendus-netcore";
+			});
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -42,8 +54,9 @@ namespace referendus_netcore
             }
 
             app.UseStaticFiles();
+			app.UseAuthentication();
 
-            app.UseMvc(routes =>
+			app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
