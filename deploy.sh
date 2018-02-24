beb87db899d4b9211db1ac280127a92a443dc54d
Grayson Gilmore <gilmoreg@live.com>
2018-02-24 14:08:45 -0800

    Remove linebreak

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e # Abort script at first error
 set -u # Disallow unset variables
-
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u=$DOCKER_USER -p=$DOCKER_PASS registry.heroku.com
