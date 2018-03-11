477631ed8184d0f6cf8326c51070bfc8d75feeb5
Grayson Gilmore <gilmoreg@live.com>
2018-03-10 19:23:56 -0800

    Use json and reflection in Edit

--- Controllers/ReferenceController.cs
+++ Controllers/ReferenceController.cs
@@ -1,8 +1,13 @@
 ﻿namespace referendus_netcore
 {
 	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Http;
 	using Microsoft.AspNetCore.Mvc;
+	using Newtonsoft.Json.Linq;
+	using System;
+	using System.Collections.Generic;
 	using System.Linq;
+	using System.Reflection;
 
 	public class ReferenceController : Controller
     {
@@ -74,16 +79,47 @@
 			return NotFound();
 		}
 
-		[HttpPatch("[action]/{id}"), Authorize]
-		public IActionResult Edit([FromBody] Reference reference, int id)
+		[HttpPut("[action]/{id}"), Authorize]
+		public IActionResult Edit([FromBody] JObject update, int id)
 		{
+			if (update == null) return BadRequest();
+
 			var userId = Helpers.GetUserId(User);
 			if (string.IsNullOrEmpty(userId))
 			{
 				return BadRequest(ErrorMessages.NoNameIdentifier);
 			}
 
-			return Ok();
+			var referenceToUpdate = _referenceData.Get(id, userId);
+			if (referenceToUpdate == null) return NotFound();
+
+			var properties = new List<PropertyInfo>(referenceToUpdate.GetType().GetProperties());
+			foreach(var field in update)
+			{
+				var propertyToUpdate = properties.FirstOrDefault(p => p.Name == field.Key);
+				if (propertyToUpdate == null)
+				{
+					return BadRequest($"Property {field.Key} cannot be updated on reference type {referenceToUpdate.GetType().Name}");
+				}
+				var value = field.Value.ToObject(typeof(object));
+				switch (value)
+				{
+					case int i:
+						propertyToUpdate.SetValue(referenceToUpdate, i);
+						break;
+					case string s:
+						propertyToUpdate.SetValue(referenceToUpdate, s);
+						break;
+					case DateTime d:
+						propertyToUpdate.SetValue(referenceToUpdate, d);
+						break;
+					default:
+						return BadRequest(ErrorMessages.InvalidPropertyType);
+				}
+			}
+
+			var result = _referenceData.Update(referenceToUpdate);
+			return Ok(result);
 		}
     }
 }
--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -4,6 +4,8 @@
     {
 		public const string InvalidFormat = "Invalid format - must be apa, chicago, or mla";
 		public const string NoNameIdentifier = "Invalid authorization - no name identifier claim present";
+		public const string InternalServerError = "Something went wrong";
+		public const string InvalidPropertyType = "Invalid property type";
 	}
 
 	public class ReferenceTypes
