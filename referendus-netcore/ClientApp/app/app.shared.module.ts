100ceff47467b84f6674464616fab678832f3212
Grayson Gilmore <gilmoreg@live.com>
2018-03-15 20:00:55 -0700

    Juggle dependencies for angular upgrade and prep for moving referendus client code over

--- referendus-netcore/ClientApp/app/app.shared.module.ts
+++ referendus-netcore/ClientApp/app/app.shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
